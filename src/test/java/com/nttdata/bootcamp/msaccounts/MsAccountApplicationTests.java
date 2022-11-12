730f6d73e00f19e09de2da0bc96a6e2e5647a2e9
TheoRevFdz <hrevillafernandez79@gmail.com>
2022-11-11 19:05:21 -0500

    .

--- src/test/java/com/nttdata/bootcamp/msaccounts/MsAccountApplicationTests.java
+++ src/test/java/com/nttdata/bootcamp/msaccounts/MsAccountApplicationTests.java
@@ -1,13 +1,39 @@
 package com.nttdata.bootcamp.msaccounts;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.List;
 
-@SpringBootTest
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.nttdata.bootcamp.msaccounts.model.Account;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MsAccountApplicationTests {
+	
+	@Autowired
+	private WebTestClient client;
 
 	@Test
-	void contextLoads() {
+	public void getAccounts() {
+		client.get()
+		.uri("/")
+		.accept(MediaType.APPLICATION_JSON)
+		.exchange()
+		.expectStatus().isOk()
+		.expectHeader().contentType(MediaType.APPLICATION_JSON)
+		.expectBodyList(Account.class)
+		.consumeWith(resp->{
+			List<Account> accounts=resp.getResponseBody();
+			accounts.forEach(a->{
+				System.out.println(a.toString());
+			});
+			
+			Assertions.assertThat(accounts.size()>0).isTrue();
+		});
 	}
 
 }
